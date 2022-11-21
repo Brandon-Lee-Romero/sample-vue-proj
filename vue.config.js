314681bc3007691e884bd76f3586619d7ec3d3f7
Scylla-27 <114054483+Scylla-27@users.noreply.github.com>
2022-11-21 15:03:00 +0800

    added config

--- vue.config.js
+++ vue.config.js
@@ -1,40 +1,32 @@
+const { defineConfig } = require('@vue/cli-service')
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-    pluginOptions: {
-        i18n: {
-            locale: 'en',
-            fallbackLocale: 'en',
-            localeDir: 'locales',
-            enableInSFC: false
-        }
+module.exports = defineConfig(
+    {
+        transpileDependencies: true
     },
-    chainWebpack: config => {
-        // Remove prefetch plugin and that's it!
-        config.plugins.delete('prefetch');
-        config.optimization.delete('splitChunks')
+    {
+        devServer: {
+            public : process.env.APP_URL,
+            host : '0.0.0.0',
+            disableHostCheck : true
+                }
     },
-    filenameHashing: false,
-
-    configureWebpack: {
-        resolve: {
-            alias: {
-                '@themeConfig': path.resolve(__dirname, 'theme.config.js'),
-            }
+    {
+        configureWebpack: {
+            resolve: {
+                alias: {
+                    '@themeConfig': path.resolve(__dirname, 'theme.config.js'),
+                }
+            },
+            plugins: [
+                new webpack.ProvidePlugin({
+                    $: "jquery",
+                    jQuery: "jquery",
+                    "window.jQuery": "jquery",
+                })
+            ]
         },
-        plugins: [
-            new webpack.ProvidePlugin({
-                $: "jquery",
-                jQuery: "jquery",
-                "window.jQuery": "jquery",
-            })
-        ]
-    },
-    devServer: {
-        public : process.env.APP_URL,
-        host : '0.0.0.0',
-        disableHostCheck : true
-    },
-    publicPath: "/"
-};
+        publicPath: "/"
+    })
