542beb3202193a7a8e49aed8b63e73d56b907bb5
Scylla-27 <114054483+Scylla-27@users.noreply.github.com>
2022-11-21 14:36:25 +0800

    added config

--- vue.config.js
+++ vue.config.js
@@ -2,50 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
    transpileDependencies: true
   
-},
-{ 
-  configureWebpack: {
-      devServer: {
-        compress: true,
-        inline: true,
-        port: '8080',
-        allowedHosts: "all",
-        disableHostCheck: true,
-        host: "0.0.0.0"
-      }
-}
-})
-
-const path = require('path');
-const webpack = require('webpack');
-module.exports = {
-    pluginOptions: {
-        i18n: {
-            locale: 'en',
-            fallbackLocale: 'en',
-            localeDir: 'locales',
-            enableInSFC: false
-        }
-    },
-    chainWebpack: config => {
-        // Remove prefetch plugin and that's it!
-        config.plugins.delete('prefetch');
-        config.optimization.delete('splitChunks')
-    },
-    filenameHashing: false,
-
-    configureWebpack: {
-        resolve: {
-            alias: {
-                '@themeConfig': path.resolve(__dirname, 'theme.config.js'),
-            }
-        },
-        plugins: [
-            new webpack.ProvidePlugin({
-                $: "jquery",
-                jQuery: "jquery",
-                "window.jQuery": "jquery",
-            })
-        ]
-    }
-};
+  },
+  {
+    devServer: {
+    proxy: 'http://localhost:4000',
+    disableHostCheck: true      
+  }
+});
